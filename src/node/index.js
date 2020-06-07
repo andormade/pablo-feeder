c0263cec3c5e10b10e09aac6fb60dcfb29cba5ea
andormade <hngrhorace@gmail.com>
2020-06-07 19:54:35 +0200

    get remote ip from heroku

--- src/node/index.js
+++ src/node/index.js
@@ -5,18 +5,22 @@ const port = process.env.PORT || 6969
 let address = '';
 let lastUpdate = 0;
 
+app.get('/', (req, res) => {
+	res.sendStatus(200)
+})
+
 app.get('/update-address', (req, res) => {
 	const {port, protocol} = req.query
+	const ip = req.header('x-forwarded-for')
 
 	if (!port || !protocol) {
 		return res.sendStatus(400)
 	}
 
-	const host = ip6addr.parse(req.connection.remoteAddress).toString({ format: 'v4' })
+	const host = ip6addr.parse(ip).toString({ format: 'v4' })
 	address = protocol + '://' + host + ':' + port
 	lastUpdate = Date.now()
-	res.send(req.header('x-forwarded-for'))
-	//res.sendStatus(200)
+	res.sendStatus(200)
 })
 
 app.get('/get-address', (req, res) => {
